584f480ffb9518c24674365646901c665a1e9f33
rangelbjordao <96200914+rangelbjordao@users.noreply.github.com>
2025-04-09 09:41:19 -0300

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Header } from "./components/header/header";
+import { Footer } from "./footer/footer";
 import "./globals.css";
 
 export default function RootLayout({
@@ -8,9 +9,12 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="pt-br">
-            <body className="font-sans text-lg m-0 p-0">
+            <body className="font-sans text-lg m-0 p-0 min-h-screen flex flex-col">
                 <Header />
+
                 {children}
+
+                <Footer />
             </body>
         </html>
     );
